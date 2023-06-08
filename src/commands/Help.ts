9c3523b863fe94fd406bdede676025b0066d8180
Liesel Downes <l_downes@icloud.com>
2023-06-08 17:09:15 +0100

    Help command

---
+++ src/commands/Help.ts
@@ -0,0 +1,31 @@
+import { Subcommand } from "@sapphire/plugin-subcommands";
+
+export class HelpCommand extends Subcommand {
+    public constructor(context: Subcommand.Context, options: Subcommand.Options) {
+        super(context, {
+            ...options,
+            name: 'help',
+            description: 'Help commands',
+            subcommands: [
+                { name: 'import-formula', chatInputRun: 'chatInputImportFormula' }
+            ]
+        });
+    }
+
+    public override registerApplicationCommands(registry: Subcommand.Registry) {
+        registry.registerChatInputCommand(
+            (builder) => 
+                builder
+                    .setName(this.name)
+                    .setDescription(this.description)
+                    .addSubcommand((command) => 
+                        command.setName('import-formula').setDescription('The input formula for MPs')
+                    ),
+            { idHints: ['1116383606248583259'] }
+        );
+    }
+
+    public async chatInputImportFormula(interaction: Subcommand.ChatInputCommandInteraction) {
+        await interaction.reply('```clean\n=ArrayFormula(concatenate("[", join(", ", char(34)&B5:B54&char(34)), "]"))\n```');
+    }
+}
