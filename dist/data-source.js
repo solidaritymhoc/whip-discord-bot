253124a2671dc780b7f759a7c8e4966e1b9e62fc
Liesel Downes <l_downes@icloud.com>
2023-07-15 12:57:58 +0930

    Back to mysql

--- dist/data-source.js
+++ dist/data-source.js
@@ -6,12 +6,15 @@ const dotenv_1 = require("dotenv");
 const isTs = require('detect-ts-node');
 (0, dotenv_1.config)();
 exports.AppDataSource = new typeorm_1.DataSource({
-    type: 'sqlite',
-    database: 'whipbot.sqlite',
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: "all",
-    entities: isTs ? ["src/entity/*.ts"] : ["entity/*.js"],
+    entities: isTs ? ["src/entity/*.ts"] : ["/dist/entity/*.js"],
     migrations: [],
     subscribers: [],
-    logger: "file",
 });
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,18 @@
-import { DataSource } from "typeorm";
-import { config } from "dotenv";
+import { DataSource } from "typeorm"
+import { config } from "dotenv"
 const isTs = require('detect-ts-node');
-config();
+config()
 
 export const AppDataSource = new DataSource({
-    type: 'sqlite',
-    database: 'whipbot.sqlite',
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: "all",
-    entities: isTs ? ["src/entity/*.ts"] : ["entity/*.js"],
+    entities: isTs ? ["src/entity/*.ts"] : ["/dist/entity/*.js"],
     migrations: [],
     subscribers: [],
-    logger: "file",
-});
+})
